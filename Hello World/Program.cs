930116592c032e536198f65e588b81cf0c3f8571
Jeremy Childers <jlchilders11@k2m.com>
2020-03-03 20:14:05 -0500

    Take in words or strings from the command line, then output using writeline

---
+++ Hello World/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(string s in args)
+            {
+                Console.WriteLine("{0}", s);
+            }
+        }
+    }
+}
